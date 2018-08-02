8b494aea6de83993820ba26f5ca620eb588f5630
CodeDotJS <rushforlinux@gmail.com>
2018-08-03 03:26:19 +0530

    update tests

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ import test from 'ava';
 import m from '.';
 
 test('user', async t => {
-	const num = await m('CodeDotJS', 'instagram-profile-picture');
+	const num = await m('CodeDotJS', 'youtube-playlist');
 
-	t.is(num.repositories, '12');
-	t.is(num.packages, '0');
+	t.is(num.repositories, '7');
+	t.is(num.packages, '2');
 });
